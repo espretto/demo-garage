230a2174fcd9d330d9ab03d8ac4b1769fc641f30
espretto <espretto@posteo.net>
2020-02-08 15:07:47 +0100

    app factory pattern for wsgi pre-fork server pickup

--- swagger_server/__main__.py
+++ swagger_server/__main__.py
@@ -5,12 +5,17 @@ import connexion
 from swagger_server import encoder
 
 
-def main():
+def create_app():
     app = connexion.App(__name__, specification_dir='./spec/')
     app.app.json_encoder = encoder.JSONEncoder
     app.add_api('garage.openapi.yml', arguments={'title': 'Stompyt Garage'})
-    app.run(port=8080)
+    return app
 
 
 if __name__ == '__main__':
-    main()
+    create_app().run(port=8080)
+else:
+    # publish wsgi app to be picked up by any
+    # pre-fork production server e.g. gunicorn
+    # application = create_app()
+    pass 
