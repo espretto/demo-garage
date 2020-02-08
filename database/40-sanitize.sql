a19d26a88d8d564887acda8fa5c8daa1ef8b7447
espretto <espretto@posteo.net>
2020-02-08 18:47:12 +0100

    fix sql aliasing

--- database/40-sanitize.sql
+++ database/40-sanitize.sql
@@ -1,9 +1,9 @@
 
 -- increase garage capacity where necessary
 update garage set max_capacity = (
-  select count(c.id) from car c where c.garage_id = garage.id
+  select count(c.id) from car as c where c.garage_id = garage.id
 ) where id in (
-  select g.id from garage g left join car c
+  select g.id from garage as g left join car as c
   on g.id = c.garage_id
   group by c.garage_id
   having count(c.id) > g.max_capacity
